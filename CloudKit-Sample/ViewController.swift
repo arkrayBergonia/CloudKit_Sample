277d9669a4b8bf88f79a056b4709ea69c79ab7a5
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2020-01-15 11:29:33 +0800

    Initial Commit

---
+++ CloudKit-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKit-Sample
+//
+//  Created by Francis Jemuel Bergonia on 1/15/20.
+//  Copyright © 2020 Arkray PHM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
